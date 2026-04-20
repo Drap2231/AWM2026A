169a8ea1daefb9c04fe864e801ec64f21f419793
Sramos13 <solramos64@outlook.com>
2026-04-20 15:58:49 -0500

    nuevaCarpeta

---
+++ 20-04-26/script.js
@@ -0,0 +1,15 @@
+/*function saludar(){
+    console.log("Hola")
+}
+saludar()*/
+
+/*const InsertarEstudiante = () => {
+    const titulo = document.getElementById("principal")
+    titulo.style.backgroundColor = "red"
+}*/
+AgregarEstudiante = () => () {
+    const divContenedor = document.getElementById("contenedor")
+    const itemEstudiante = document.createElement
+    itemEstudiante.inner.Text="ok"
+    divContenedor.appendChild(itemEstudiante)
+}
