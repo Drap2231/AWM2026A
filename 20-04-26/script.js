5ce63bcec64f90b799111eec40dfeb49c54790cd
Drap2231 <dcarrion1302@gmail.com>
2026-04-27 08:22:55 -0500

    Cambios y agregacion de Carpetas

--- 20-04-26/script.js
+++ 20-04-26/script.js
@@ -7,9 +7,9 @@ saludar()*/
     const titulo = document.getElementById("principal")
     titulo.style.backgroundColor = "red"
 }*/
-AgregarEstudiante = () =>  {
+agregarEstudiante = () =>  {
     const divContenedor = document.getElementById("contenedor")
     const itemEstudiante = document.createElement("p")
-    itemEstudiante.inner.Text="ok"
+    itemEstudiante.innerText="ok"
     divContenedor.appendChild(itemEstudiante)
 }
