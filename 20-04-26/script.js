044a3886038553f2cbe2d14cc52bdac06dcc1cfb
Sramos13 <solramos64@outlook.com>
2026-04-27 14:02:52 -0500

    ok

--- 20-04-26/script.js
+++ 20-04-26/script.js
@@ -7,9 +7,26 @@ saludar()*/
     const titulo = document.getElementById("principal")
     titulo.style.backgroundColor = "red"
 }*/
-AgregarEstudiante = () => () {
-    const divContenedor = document.getElementById("contenedor")
-    const itemEstudiante = document.createElement
-    itemEstudiante.inner.Text="ok"
-    divContenedor.appendChild(itemEstudiante)
+/*var estudiante = {id :1, nombre, nombre:"ok ok"}*/
+const lstEstudiantes = [{id:1, nombre: "Anaela"},{id: 2,nombre:"Daniel"}]
+
+/*lstEstudiantes.forEach(estudiante => {
+    console.log(estudiante);
+});*/
+
+
+const renderizarListaEstudiantes = (event) =>  {
+    const divContenedor = document.getElementById("contenedor");
+    divContenedor.innerText = ""
+    lstEstudiantes.forEach((estudiante) => {
+        const itemEstudiante = document.createElement("p");
+        itemEstudiante.innerText= estudiante.nombre;
+        divContenedor.appendChild(itemEstudiante);
+    });
+}
+const insertarEstudiante = () => {
+    event.preventDefault()
+    const nombreIngresado = document.getElementById("txtnombre").value
+    lstEstudiantes.push({id: O, nombre : nombreIngresado})
+    renderizarListaEstudiantes()
 }
