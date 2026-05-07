0ac2b6424b4a90854fd47d06311a87b4b51a6234
Sramos13 <solramos64@outlook.com>
2026-05-07 15:59:35 -0500

    Edicion de Estado Fin

--- 04-05-26/frontend/src/paginas/EstudiantePagina.jsx
+++ 04-05-26/frontend/src/paginas/EstudiantePagina.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Estudiante from "../componentes/Estudiante";
+import {listaEstudiantes}
+ from "../utilidades/data";
 const EstudiantePagina = () => {
+    const [lstEstudiantes,setlstEstudiantes] = useState(listaEstudiantes);
+    const [nuevoEstudiante, setNuevoEstudiante]=useState({
+      id:Date.now(),
+      nombre: "",
+      edad: 0,
+      url: ""
+    })
+    const handlerAgregarEstudiante = (e) =>{
+      e.preventDefault();
+       setlstEstudiantes([...lstEstudiantes, nuevoEstudiante]); 
+       setNuevoEstudiante({...nuevoEstudiante, nombre:"", edad:0, url:""})
+    }
     return(
     <div>
-      <Estudiante 
+     {/* <Estudiante 
       nombre={"Daniel Carrion"} 
       edad= {23} 
       url = {"https://www.google.com"}
@@ -17,6 +32,36 @@ const EstudiantePagina = () => {
       edad= {50} 
       url = {"https://www.google.com"}
       />
+     */}
+     <h1>Estudiantes</h1>
+     <form onSubmit={handlerAgregarEstudiante}>
+      <div>
+        <label htmlFor="est_Nombre">Nombre: </label>
+        <input type="text" name= "est_Nombre" id = "est_Nombre" value={nuevoEstudiante.nombre} onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, nombre: e.target.value}) } placeholder="Ingresa Nombre" required/>
+      </div>
+      <div>
+        <label htmlFor="est_edad">Edad: </label>
+        <input type="number" name= "est_edad" id = "est_edad" value={nuevoEstudiante.edad}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, edad: e.target.value})} placeholder="Ingresa Edad" required/>
+      </div>
+      <div>
+        <label htmlFor="est_url">Url: </label>
+        <input type="text" name= "est_url" id = "est_url" value={nuevoEstudiante.url}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, url: e.target.value})} placeholder="Ingresa url Home page" required/>
+      </div>
+      <div>
+        <input type="submit" value="Agregar" />
+      </div>
+     </form>
+     {
+     lstEstudiantes.map((estudiante)=>{
+      return <Estudiante
+        key= {estudiante.id}
+        nombre={estudiante.nombre}
+        edad= {estudiante.edad}
+        url = {estudiante.url}
+      />
+     })
+    }
+
     </div>
     );
 }
--- 04-05-26/frontend/src/utilidades/data.js
+++ 04-05-26/frontend/src/utilidades/data.js
@@ -1,6 +1,6 @@
-const listaEstudiantes = [
-    {id = 1, nombre="Daniel Carrion", edad= 23, url = "https://www.google.com"},
-    {id = 2, nombre="Anaela Pozo", edad= 21, url = "https://www.google.com"},
-    {id = 2, nombre="Anaela Pozo", edad= 21, url = "https://www.google.com"},
-    {id = 2, nombre="Anaela Pozo", edad= 21, url = "https://www.google.com"},
+export const listaEstudiantes = [
+    {id : 1, nombre:"Daniel Carrion", edad: 23, url : "https://www.google.com"},
+    {id : 2, nombre:"Anaela Pozo", edad: 21, url : "https://www.google.com"},
+    {id : 3, nombre:"Anaela Pozo", edad: 21, url : "https://www.google.com"},
+    {id : 4, nombre:"Anaela Pozo", edad: 21, url : "https://www.google.com"},
 ]
