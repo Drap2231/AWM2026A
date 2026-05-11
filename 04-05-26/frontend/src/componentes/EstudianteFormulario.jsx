45ed278859b18a281c3e704f9f9cb7b70d9e738b
Sramos13 <solramos64@outlook.com>
2026-05-11 16:02:47 -0500

    Modularizacion

---
+++ 04-05-26/frontend/src/componentes/EstudianteFormulario.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+const EstudianteFormulario = (props) => {
+    const { onAgregar } = props;
+    const [nuevoEstudiante, setNuevoEstudiante]=useState({
+      id:"",
+      nombre: "",
+      edad: 0,
+      url: ""
+    })
+    const [errorNombre,setErrorNombre] = useState("")
+    const [errorEdad,setErrorEdad] = useState("")
+    const handlerSubmit = (e) =>{
+       e.preventDefault();
+      if((nuevoEstudiante.nombre.length>=8)&& (nuevoEstudiante.edad.value>=18)){
+       
+        onAgregar(nuevoEstudiante)
+        setErrorNombre("")
+        setErrorEdad("")
+      }
+      else{
+        setErrorNombre("oK")
+         setErrorEdad("oki")
+      }
+    }
+    
+    return (
+     <form onSubmit={handlerSubmit}>
+      <div>
+
+        <label htmlFor="est_Nombre">Nombre: </label>
+        <input type="text" name= "est_Nombre" id = "est_Nombre" value={nuevoEstudiante.nombre} onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, nombre: e.target.value}) } placeholder="Ingresa Nombre" required/>
+      <div>
+         {errorNombre}
+      </div>
+      </div>
+      <div>
+        <label htmlFor="est_edad">Edad: </label>
+        <input type="number" name= "est_edad" id = "est_edad" value={nuevoEstudiante.edad}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, edad: e.target.value})} placeholder="Ingresa Edad" required/>
+      <div>
+         {errorEdad}
+      </div>
+      </div>
+      <div>
+        <label htmlFor="est_url">Url: </label>
+        <input type="text" name= "est_url" id = "est_url" value={nuevoEstudiante.url}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, url: e.target.value})} placeholder="Ingresa url Home page" required/>
+      </div>
+      <div>
+        <input type="submit" value="Agregar" />
+      </div>
+     </form>
+
+    )
+}
+export default EstudianteFormulario
--- 04-05-26/frontend/src/paginas/EstudiantePagina.jsx
+++ 04-05-26/frontend/src/paginas/EstudiantePagina.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import Estudiante from "../componentes/Estudiante";
-import {listaEstudiantes}
- from "../utilidades/data";
+import {listaEstudiantes} from "../utilidades/data";
+ import EstudianteFormulario from "../componentes/EstudianteFormulario";
 const EstudiantePagina = () => {
+  //1er paso
     const [lstEstudiantes,setlstEstudiantes] = useState(listaEstudiantes);
-    const [nuevoEstudiante, setNuevoEstudiante]=useState({
-      id:Date.now(),
-      nombre: "",
-      edad: 0,
-      url: ""
-    })
-    const handlerAgregarEstudiante = (e) =>{
-      e.preventDefault();
-       setlstEstudiantes([...lstEstudiantes, nuevoEstudiante]); 
-       setNuevoEstudiante({...nuevoEstudiante, nombre:"", edad:0, url:""})
+    
+    console.log("renderizando...")
+    const agregarEstudiante = (nuevoEstudiante) =>{
+      //Temporalmente el ID SE CONSTRUYE CON LA FECHA ACTUAL DEL SISTEMA
+      const estudianteFinal = {...nuevoEstudiante,id:Date.now()}
+       setlstEstudiantes(prev => [...prev, estudianteFinal]); 
+        
     }
+
     return(
     <div>
      {/* <Estudiante 
@@ -33,25 +32,11 @@ const EstudiantePagina = () => {
       url = {"https://www.google.com"}
       />
      */}
-     <h1>Estudiantes</h1>
-     <form onSubmit={handlerAgregarEstudiante}>
-      <div>
-        <label htmlFor="est_Nombre">Nombre: </label>
-        <input type="text" name= "est_Nombre" id = "est_Nombre" value={nuevoEstudiante.nombre} onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, nombre: e.target.value}) } placeholder="Ingresa Nombre" required/>
-      </div>
-      <div>
-        <label htmlFor="est_edad">Edad: </label>
-        <input type="number" name= "est_edad" id = "est_edad" value={nuevoEstudiante.edad}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, edad: e.target.value})} placeholder="Ingresa Edad" required/>
-      </div>
-      <div>
-        <label htmlFor="est_url">Url: </label>
-        <input type="text" name= "est_url" id = "est_url" value={nuevoEstudiante.url}  onChange={(e)=> setNuevoEstudiante({...nuevoEstudiante, url: e.target.value})} placeholder="Ingresa url Home page" required/>
-      </div>
-      <div>
-        <input type="submit" value="Agregar" />
-      </div>
-     </form>
+    <h1>Estudiantes</h1>
+    <EstudianteFormulario onAgregar = {agregarEstudiante}/>
+    <hr />
      {
+      
      lstEstudiantes.map((estudiante)=>{
       return <Estudiante
         key= {estudiante.id}
