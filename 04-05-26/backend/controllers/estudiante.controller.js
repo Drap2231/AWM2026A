f20441e7869d41955f9463f8d589793dad93b854
Drap2231 <solramos64@outlook.com>
2026-06-10 16:01:14 -0500

    base relacional

---
+++ 04-05-26/backend/controllers/estudiante.controller.js
@@ -0,0 +1,13 @@
+const Estudiante = require("../models/estudiante.model");
+
+module.exports.getAllEstudiantes = (_,response) => {
+    Estudiante.find({})
+        .then (estudiantes => response.json(estudiantes))
+        .catch (err => response.json(err))
+}
+module.exports.getEstudianteID = (request,response) => {
+    
+    Estudiante.find({})
+        .then (estudiantes => response.json(estudiantes))
+        .catch (err => response.json(err))
+}
--- 04-05-26/backend/server.js
+++ 04-05-26/backend/server.js
@@ -1,24 +1,13 @@
+require ("./config/mongoose.config")
 const express = require("express")
 const app = express()
 const puerto = 8000
+const allEstudianteRoutes = require("./routes/estudiante.routes");
 /* res = es un objeto que contiene los metodos para responder a un la solicitud del cliente
 _ para cuando no ocupamos alguno
 Debe existir un cierre en el ciclo solicitud respuesta
 */
-app.get("/", (req,  res)=> {
-    console.log ("ejecutando endpoint")
-    res.send ("HOLA MUNDO XDXD")
-})
-app.get("/estudiantes", (req,  res)=> {
-    res.json ({"mensaje":"endpoint para obtener lista de estudiantes"})
-} )
- app.get ("/estudiantes/:id/detalle",(req,  res)=> {
-    const {id} = req.params
-    res.json ({"mensaje":`endpoint para obtener estudiante con id ${id}`})
-} )
-app.delete("/estudiantes", (req,  res)=> {
-    res.json ({"mensaje":"endpoint para obtener para eliminar"})
-} )
+allEstudianteRoutes(app);
 app.listen (puerto, ()=>console.log("El servidor esta escuchando en el puerto:", puerto))
 /* Carpeta config
 El proposito pricipal del mongus.config.js es para establecer la cadena de conexion(lugar donde se encuentra, credenciales, puerto) con el mongosdb
