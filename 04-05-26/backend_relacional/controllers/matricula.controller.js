d8881a97006d2a4d16f4aeba6c5f9b26f0dc9e24
Drap2231 <solramos64@outlook.com>
2026-07-06 15:50:45 -0500

    Cliente Movil

--- 04-05-26/backend_relacional/controllers/matricula.controller.js
+++ 04-05-26/backend_relacional/controllers/matricula.controller.js
@@ -1,38 +1,4 @@
 const Matricula = require('../models/matricula.model');
-<<<<<<< HEAD
-//const Estudiante = require('../models/estudiante.model');
-const Asignatura = require('../models/asignatura.model');
-const { where } = require('sequelize');
-
-// Controlador para matricular un estudiante en una asignatura
-module.exports.matricularEstudiante = async (req, res) => {
-    try {
-        const { estudianteId, asignaturaId } = req.body;
-
-        const matricula = await Matricula.create({
-            EstudianteId: estudianteId,
-            AsignaturaId: asignaturaId
-        });
-
-        res.json(matricula);
-    } catch (err) {
-        res.status(500).json({
-            msg: "Ocurrió un error al registrar la matrícula"
-        });
-    }
-};
-module.exports.getEstudianteMatricula = async (req, res) => {
-    try{
-        const matricula = await Matricula.findAll({
-            where : {EstudianteId: req.params.estudianteId},
-            include: [Asignatura]
-        }) ;
-        res.json(matricula);
-    } catch (err) {
-        res.status(500).json({msg: "Ocurrio un error al obtner las asignaturas matriculadas" })
-    }
-    }
-=======
 const Estudiante = require('../models/estudiante.model');
 const Asignatura = require('../models/asignatura.model');
 
@@ -66,4 +32,3 @@ module.exports = {
         }
     }
 };
->>>>>>> 4568d6eaee5009e4cfa96ab71fdb86bac0398593
